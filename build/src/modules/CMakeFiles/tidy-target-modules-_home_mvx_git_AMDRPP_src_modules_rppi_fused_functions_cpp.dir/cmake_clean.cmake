c35bf8d712d27d4ee7b6f8a8610af3c1d25592bc
Reza-Najafi <seyedreza.najafi@amd.com>
2020-02-25 12:49:37 -0800

    removed build folder

--- build/src/modules/CMakeFiles/tidy-target-modules-_home_mvx_git_AMDRPP_src_modules_rppi_fused_functions_cpp.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/tidy-target-modules-_home_mvx_git_AMDRPP_src_modules_rppi_fused_functions_cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/tidy-target-modules-_home_mvx_git_AMDRPP_src_modules_rppi_fused_functions_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/src/modules/rpp_kernel_includes.h
+++
@@ -1,4 +0,0 @@
-#ifndef GUARD_RPP_KERNEL_INCLUDES_HPP_
-#define GUARD_RPP_KERNEL_INCLUDES_HPP_
-#include <stddef.h>
-#endif
